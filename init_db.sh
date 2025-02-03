e87f8a5f2555380a54d772733695a74eb2d5f25a
B4rb4r1s <vgokarev@mail.ru>
2025-02-03 14:26:34 +0300

    Update DB

--- init_db.sh
+++ init_db.sh
@@ -5,17 +5,16 @@ psql -U happy_user -d happy_db <<-EOSQL
             ID SERIAL PRIMARY KEY,
             filename VARCHAR,
             content TEXT,
+            big_summary TEXT,
             summary TEXT,
             upload_time TIMESTAMP
         );
-
         CREATE TABLE NAMED_ENTITIES(
             ID SERIAL PRIMARY KEY,
             doc_id INTEGER,
             entity VARCHAR,
             value VARCHAR
         );
-
         CREATE TABLE METADATA(
             ID SERIAL PRIMARY KEY,
             doc_id INTEGER,
@@ -29,13 +28,34 @@ psql -U happy_user -d happy_db <<-EOSQL
             producer VARCHAR
         );
 
+
         CREATE TABLE DOC_DATASET(
             ID SERIAL PRIMARY KEY,
             full_text_tesseract VARCHAR,
             full_text_dedok VARCHAR,
             file_name VARCHAR,
             event VARCHAR,
-            format VARCHAR
+            format VARCHAR,
+            big_summary TEXT,
+            summary TEXT,
+        );      
+        CREATE TABLE NAMED_ENTITIES_DATASET(
+            ID SERIAL PRIMARY KEY,
+            doc_id INTEGER,
+            entity VARCHAR,
+            value VARCHAR
+        );
+        CREATE TABLE METADATA_DATASET(
+            ID SERIAL PRIMARY KEY,
+            doc_id INTEGER,
+            format VARCHAR,
+            author VARCHAR,
+            creator VARCHAR,
+            title VARCHAR,
+            subject VARCHAR,
+            keywords VARCHAR,
+            creation_date TIMESTAMP,
+            producer VARCHAR
         );
 
     COMMIT;
